95cd84e95c648da781f94f3174d8a63cd1d5936d
Melissa Fields <melissamfields12@gmail.com>
2022-11-17 14:19:17 -0800

    Revise JS to try and render one random fact.

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 const card = document.querySelector('.card');
 // let randomFact = Math.floor(Math.random() * (fact.length))
+const factUrl = 'http://localhost:3000/facts'
 let factArray = []
 
 function fetchFacts() {
-    fetch('http://localhost:3000/facts')
+    fetch(factUrl)
     .then(resp => resp.json())
-    .then(data => {
-        factArray = data.map(fact => showFact(fact));  
-        console.log(fact)
+    .then(facts => { 
+        factArray = Object.values(facts)
+        factArray.forEach((fact) => {
+            // console.log(factArray)
+        createFact(factArray)
+        }) 
+        // debugger;
+        // data.map(fact => showFact(fact));  
+        // console.log(fact)
     })
+}
 fetchFacts()
 
-function showFact(fact) {
+function createFact(factArray) {
+    factArray.map((facts) => {
+    let randomFact = Math.floor(Math.random() * (factArray.length))
     const ul = document.createElement('ul')
     ul.classList.add('list')
     const li1 = document.createElement('li');
-    li1.textContent = `Number: ${fact.number}`;
+    li1.textContent = `Number: ${facts.number}`;
     const li2 = document.createElement('li');
-    li2.textContent = `Random fact: ${fact.text}`;
+    li2.textContent = `Random fact: ${facts.text}`;
     const li3 = document.createElement('li');
-    li3.textContent = `Fact type: ${fact.type}`;
+    li3.textContent = `Fact type: ${facts.type}`;
     ul.append(li1, li2, li3)
     card.append(ul);
-    console.log(fact)
-    const factID = fact.id;
-    factID(Math.floor(Math.random() * (factID.length)))
+    
+    // console.log(factArray)
+    // const factID = fact.id;
+    // factID(Math.floor(Math.random() * (factID.length)))
+})
 }
