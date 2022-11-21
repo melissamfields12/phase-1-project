910a7f024c474cb0e03e1676a3c7c67a5ca874be
Melissa Fields <melissamfields12@gmail.com>
2022-11-20 21:31:56 -0800

    Revise JavaScript code to generate one random fact.

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 const card = document.querySelector('.card');
-// let randomFact = Math.floor(Math.random() * (fact.length))
 const factUrl = 'http://localhost:3000/facts'
 let factArray = []
+
+// Create a mouseover event listener when hovering over the daily fact
+// to create a bolder border
+
+// Create click event to generate a new random fact
+
+// Create submit event listener to prevent refreshing the page when typing
+// in a number they want to view a fact about
+
 // Fetch JSON data
 function fetchFacts() {
     fetch(factUrl)
     .then(resp => resp.json())
-    .then(facts => { //console.log(facts)
-        factArray = Object.values(facts)
-        // factArray.forEach((fact) => {
-            // console.log(factArray)
-        createFact(factArray)
-        }) 
-        // debugger;  
-
+    .then(factArray =>  {
+    const generatedFact = generateRandomFact(factArray)
+    createFact(generatedFact)
+    })
 }
 fetchFacts()
-// Create elements to store fact information and append to the DOM
-// Single out one random fact
-function createFact(factArray) {
-    factArray.map((facts) => {
-    let randomFact = Math.floor(Math.random() * (factArray.length))
-    const ul = document.createElement('ul')
-    ul.classList.add('list')
-    const li1 = document.createElement('li');
-    li1.textContent = `Number: ${facts.number}`;
-    const li2 = document.createElement('li');
-    li2.textContent = `Random fact: ${facts.text}`;
-    const li3 = document.createElement('li');
-    li3.textContent = `Fact type: ${facts.type}`;
-    ul.append(li1, li2, li3)
-    card.append(ul);
-    
-    // const factID = fact.id;
-    // factID(Math.floor(Math.random() * (factID.length)))
-})
+
+// Single out one random fact to appear and add delete
+function generateRandomFact(factArray) {
+    let randomNumber = Math.floor(Math.random() * (factArray.length));
+    const randomFact = factArray[randomNumber];
+    return randomFact;
 }
+// Create elements to store fact information and append to the DOM
+function createFact(generatedFact){  
+        const ul = document.createElement('ul')
+        ul.classList.add('list')
+        const li1 = document.createElement('li');
+        li1.textContent = `Number: ${generatedFact.number}`;
+        const li2 = document.createElement('li');
+        li2.textContent = `Random fact: ${generatedFact.text}`;
+        const li3 = document.createElement('li');
+        li3.textContent = `Fact type: ${generatedFact.type}`;
+        ul.append(li1, li2, li3)
+        card.append(ul); 
+}
+    
+
+
+// Create a function that will allow users to type in a number to view the
+// fact associated. 
