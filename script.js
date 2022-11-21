16d2880bb43be5e61fe6a9118287d489f574ed16
Melissa Fields <melissamfields12@gmail.com>
2022-11-20 22:19:10 -0800

    Add event listener and JavaScript function for click event.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const card = document.querySelector('.card');
 const factUrl = 'http://localhost:3000/facts'
+const newFactBtn = document.querySelector('#new-fact-btn')
 let factArray = []
 
 // Create a mouseover event listener when hovering over the daily fact
 // to create a bolder border
 
 // Create click event to generate a new random fact
+newFactBtn.addEventListener("click", generateNewFact); 
 
 // Create submit event listener to prevent refreshing the page when typing
 // in a number they want to view a fact about
@@ -20,7 +22,6 @@ function fetchFacts() {
     })
 }
 fetchFacts()
-
 // Single out one random fact to appear and add delete
 function generateRandomFact(factArray) {
     let randomNumber = Math.floor(Math.random() * (factArray.length));
@@ -29,19 +30,21 @@ function generateRandomFact(factArray) {
 }
 // Create elements to store fact information and append to the DOM
 function createFact(generatedFact){  
-        const ul = document.createElement('ul')
-        ul.classList.add('list')
-        const li1 = document.createElement('li');
-        li1.textContent = `Number: ${generatedFact.number}`;
-        const li2 = document.createElement('li');
-        li2.textContent = `Random fact: ${generatedFact.text}`;
-        const li3 = document.createElement('li');
-        li3.textContent = `Fact type: ${generatedFact.type}`;
-        ul.append(li1, li2, li3)
-        card.append(ul); 
+    const ul = document.createElement('ul')
+    ul.classList.add('list')
+    const li1 = document.createElement('li');
+    li1.textContent = `Number: ${generatedFact.number}`;
+    const li2 = document.createElement('li');
+    li2.textContent = `Random fact: ${generatedFact.text}`;
+    const li3 = document.createElement('li');
+    li3.textContent = `Fact type: ${generatedFact.type}`;
+    ul.append(li1, li2, li3)
+    card.append(ul);    
 }
-    
-
-
+// Create function to generate new fact when the click event listener's clicked
+function generateNewFact() {
+    card.innerHTML = "";
+    fetchFacts()
+}
 // Create a function that will allow users to type in a number to view the
 // fact associated. 
